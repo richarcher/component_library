a166761afc62047338923706a6930e2908906820
Rich Archer <rich.archer@unboxedconsulting.com>
2016-03-09 12:45:19 +0200

    Configurable application stylesheet with default to ‘application’

--- lib/component_library.rb
+++ lib/component_library.rb
@@ -2,9 +2,10 @@ require "component_library/engine"
 
 module ComponentLibrary
   class << self
-    mattr_accessor :root_directory, :root_path
+    mattr_accessor :root_directory, :root_path, :application_css
     self.root_directory = "component"
     self.root_path = "components"
+    self.application_css = "application"
   end
 
   def self.setup(&block)
