1636c33516c838c56751543ddeb89fd0946ed5bd
Rich Archer <rich.archer@unboxedconsulting.com>
2016-04-20 14:52:59 +0200

    explicit sorting

--- app/controllers/library_controller.rb
+++ app/controllers/library_controller.rb
@@ -2,13 +2,13 @@ class LibraryController < ApplicationController
   layout "component_library"
 
   def index
-    dir = Dir.glob("#{Rails.root}/app/views/#{ComponentLibrary.root_directory}/**/*.erb")
+    dir = Dir.glob("#{Rails.root}/app/views/#{ComponentLibrary.root_directory}/**/*.erb").sort
     @library = Library::Librarian.new(dir, ComponentLibrary.root_directory)
   end
 
   def show
     suffix = params[:format] == 'erb' ? ".erb" : "/**/*.erb"
-    dir = Dir.glob("#{Rails.root}/app/views/#{ComponentLibrary.root_directory}/#{params[:path]}#{suffix}")
+    dir = Dir.glob("#{Rails.root}/app/views/#{ComponentLibrary.root_directory}/#{params[:path]}#{suffix}").sort
     @library = Library::Librarian.new(dir, ComponentLibrary.root_directory, params[:path])
     raise ActionController::RoutingError.new('Not Found') if @library.components.empty?
   end
