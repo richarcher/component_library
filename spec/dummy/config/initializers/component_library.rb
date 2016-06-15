1eeb96925afe15e641ab211d09b8d2f6cf824219
Rich Archer <rich.archer@unboxedconsulting.com>
2016-06-15 15:26:45 +0200

    Creating a simple example where 2 libraries are created

--- spec/dummy/config/initializers/component_library.rb
+++ spec/dummy/config/initializers/component_library.rb
@@ -1,3 +1,8 @@
 ComponentLibrary.configure do |config|
   config.root_directory = "theme_components"
 end
+
+ComponentLibrary.configure do |config|
+  config.root_directory = "theme_components"
+  config.application_css = "themed_application"
+end
